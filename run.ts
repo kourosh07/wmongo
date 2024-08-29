fd7fce698490a3ff286bbce81027235ddcebe5c3
arminkardan <$armin.fire@gmail.com>
2024-08-29 15:00:22 +0330

    Update

---
+++ run.ts
@@ -0,0 +1,33 @@
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+import fs from 'fs'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+import path from 'path'
+
+const envPath = path.resolve(__dirname, '.env.local');
+if (fs.existsSync(envPath)) {
+    dotenv.config({ path: envPath });
+};
+
+(async () => {
+
+    const client = new MongoClient(process.env.MONGOURL);
+
+    try {
+        await client.connect();
+        console.log('Connected to the MongoDB server');
+        const db = client.db(process.env.MONGODB_DB);
+
+
+        let collections = await db.collections();
+        console.log(collections.map(c=> c.collectionName))
+        
+
+    } catch {
+        console.log("Failed to connect to Mongo Server.")
+    }
+})()
+
+
+
